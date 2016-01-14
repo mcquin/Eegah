781bc2b20e22b839b1b91f99425be777dfdc5602
mcquin <claire@chef.io>
2016-01-14 08:42:34 -0800

    initial cookbook commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'Eegah'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures Eegah'
+long_description 'Installs/Configures Eegah'
+version '0.1.0'
